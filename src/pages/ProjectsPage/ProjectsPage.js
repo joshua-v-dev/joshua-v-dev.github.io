90ab8bd9d6f4dde68802759077f2de60ff5fbfe3
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-22 23:17:11 -0500

    removed Link from ProjectsPage

--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 import { projects } from '../../constants/constants'
 
-const ProjectsPage = () => <Link href={'/[slug]'}></Link>
-projects.map((projects) => {
-	return projects.slug
-})
+const ProjectsPage = () =>
+	projects.map((projects) => {
+		return projects.slug
+	})
 
 export default ProjectsPage
