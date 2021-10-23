ae3c63b8e9183f21e4da5fc7b084e40f981de3d4
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-22 19:00:32 -0500

    rewrote function ProjectsPage to include getStaticPaths

--- src/pages/ProjectsPage/index.js
+++ src/pages/ProjectsPage/index.js
@@ -1,6 +1,13 @@
+import Link from 'next/link'
 import { projects } from '../../constants/constants'
 
-const ProjectsPage = () =>
+export const getStaticProps = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users')
+	const data = await res.json()
+
+	return props.ProjectsPage.data
+}
+const ProjectsPage = ({ projects }) =>
 	projects.map((projects) => {
 		return projects.id
 	})
