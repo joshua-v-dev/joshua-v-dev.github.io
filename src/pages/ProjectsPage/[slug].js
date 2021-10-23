7d80353d10d4c4abf7c2041a9de45e2b42f6dd23
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-22 21:35:28 -0500

    fixed staticpros error in slug.js

--- src/pages/ProjectsPage/[slug].js
+++ src/pages/ProjectsPage/[slug].js
@@ -1,6 +1,13 @@
 import React from 'react'
 import fs from 'fs'
 
+export const getStaticProps = async () => {
+	const res = await fetch('https://jsonplaceholder.typicode.com/users')
+	const data = await res.json()
+
+	return props.ProjectsPage.data
+}
+
 export const getStaticPaths = async () => {
 	const files = fs.readdirSync('ProjectsPage')
 	console.log(files)
--- src/pages/ProjectsPage/index.js
+++ src/pages/ProjectsPage/index.js
@@ -1,12 +1,6 @@
 import Link from 'next/link'
 import { projects } from '../../constants/constants'
 
-export const getStaticProps = async () => {
-	const res = await fetch('https://jsonplaceholder.typicode.com/users')
-	const data = await res.json()
-
-	return props.ProjectsPage.data
-}
 const ProjectsPage = () => <Link href={'/[slug]'}></Link>
 projects.map((projects) => {
 	return projects.slug
