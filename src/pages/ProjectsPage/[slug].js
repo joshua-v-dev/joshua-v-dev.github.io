3cda8934dec27b5c5f840f8af31353f7a7984470
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-24 03:43:51 -0500

    redid slug

--- src/pages/ProjectsPage/[slug].js
+++ src/pages/ProjectsPage/[slug].js
@@ -2,39 +2,50 @@ import React from 'react'
 import fs from 'fs'
 import path from 'path'
 import matter from 'gray-matter'
+import Head from 'next/head'
 import marked from 'marked'
 
-export const getStaticProps = async ({ params: { slug } }) => {
-	const projects = fs.readFileSync(path.join('ProjectsPage', slug + '.js')).toString()
-	const parsedProjects = matter(projects)
-
-	const htmlString = marked(parsedProjects.content)
-
-	return { props: { htmlString } }
-}
-const Details = ({ htmlString }) => {
+const ProjectDetails = ({ htmlString, data }) => {
 	return (
 		<>
-			<div dangerouslySetInnerHTML={{ __html: htmlString }}>
-				<h1>Projects Page</h1>
-				<pre>{projects}</pre>
-			</div>
+			<Head>
+				<title>{data.title}</title>
+				<meta title='description' content={data.description} />
+			</Head>
+			<div dangerouslySetInnerHTML={{ __html: htmlString }} />
 		</>
 	)
 }
 
 export const getStaticPaths = async () => {
 	const files = fs.readdirSync('ProjectsPage')
-	console.log(files)
+	console.log('files: ', files)
 	const paths = files.map((filename) => ({
 		params: {
 			slug: filename.replace('.js', ''),
 		},
 	}))
+	console.log('paths: ', paths)
+
 	return {
 		paths,
 		fallback: false,
 	}
 }
 
-export default Details
+export const getStaticProps = async ({ params: { slug } }) => {
+	const markdownWithMetadata = fs.readFileSync(path.join('ProjectsPage', slug + '.js')).toString()
+
+	const parsedMarkdown = matter(markdownWithMetadata)
+
+	const htmlString = marked(parsedMarkdown.content)
+
+	return {
+		props: {
+			htmlString,
+			data: parsedMarkdown.data,
+		},
+	}
+}
+
+export default ProjectDetails
