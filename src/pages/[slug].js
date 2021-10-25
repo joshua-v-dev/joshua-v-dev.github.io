d0f0386ee306250a943654263376f53ac282a9ba
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-25 16:53:26 -0500

    changed readdirsync

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -29,7 +29,7 @@ export const getStaticProps = async ({ params: { slug } }) => {
 }
 
 export const getStaticPaths = async () => {
-	const files = fs.readdirSync('ProjectPages')
+	const files = fs.readdirSync('constants')
 	console.log('files: ', files)
 	const paths = files.map((filename) => ({
 		params: {
