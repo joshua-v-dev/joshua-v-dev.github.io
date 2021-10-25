94ac444b9af8c8cc2e540cd838f2c5ca6ecc1604
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-25 15:10:36 -0500

    [slug] work

--- src/pages/ProjectsPage/[slug].js
+++ src/pages/ProjectsPage/[slug].js
@@ -1,4 +1,8 @@
 import React from 'react'
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import marked from 'marked'
 import Head from 'next/head'
 
 const Details = ({ htmlString, data }) => {
@@ -13,6 +17,29 @@ const Details = ({ htmlString, data }) => {
 			<div dangerouslySetInnerHTML={{ __html: htmlString }} />
 		</>
 	)
+}
+
+export const getStaticProps = async ({ params: { slug } }) => {
+	const projects = fs.readFileSync(path.join('MernProject', slug + '.js')).toString()
+	const parsedProjects = matter(projects)
+
+	const htmlString = marked(parsedProjects.content)
+
+	return { props: { htmlString } }
+}
+
+export const getStaticPaths = async () => {
+	const files = fs.readdirSync('ProjectsPage')
+	const paths = files.map((filename) => ({
+		params: {
+			slug: filename.replace('.js', ''),
+		},
+	}))
+
+	return {
+		paths,
+		fallback: false,
+	}
 }
 
 export default Details
