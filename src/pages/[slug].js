40deac5b44429b107f3b3936917a25e52ef528fa
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-25 21:24:14 -0500

    mod slug

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -29,7 +29,7 @@ export const getStaticProps = async ({ params: { slug } }) => {
 }
 
 export const getStaticPaths = async () => {
-	const files = fs.readdirSync('')
+	const files = fs.readdirSync('ProjectPages')
 	console.log('files: ', files)
 	const paths = files.map((filename) => ({
 		params: {
