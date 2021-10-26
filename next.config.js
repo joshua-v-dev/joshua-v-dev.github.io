1fa5953977caccc5f539379a201d5ac84994da19
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-26 10:50:31 -0500

    set env to ROOT = _dirname

--- next.config.js
+++ next.config.js
@@ -1,17 +1,17 @@
-module.exports = {
-	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-		config.node = {
-			fs: 'empty', // This is required
-		}
-		return config
-	},
-
-	async rewrites() {
-		return [
-			{
-				source: '/ProjectsPage',
-				destination: '/MernProject',
-			},
-		]
-	},
+export function webpack(config, { buildId, dev, isServer, defaultLoaders, webpack }) {
+	config.node = {
+		fs: 'empty', // This is required
+	}
+	return config
+}
+export async function rewrites() {
+	return [
+		{
+			source: '/ProjectsPage',
+			destination: '/MernProject',
+		},
+	]
+}
+export const env = {
+	ROOT: __dirname,
 }
