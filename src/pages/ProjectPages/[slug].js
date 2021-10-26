95987d9f66116774ae6b93b17843684a19b97ab5
joshua-v-dev <joshua-v-dev@gmail.com>
2021-10-25 22:00:54 -0500

    moved slug to projectpages folder

--- src/pages/ProjectPages/[slug].js
+++ src/pages/ProjectPages/[slug].js
@@ -25,7 +25,7 @@ export const getStaticProps = async ({ params: { slug } }) => {
 }
 
 export const getStaticPaths = async () => {
-	const files = fs.readdirSync('filename')
+	const files = fs.readdirSync('')
 	console.log('files: ', files)
 	const paths = files.map((filename) => ({
 		params: {
