fb7ca24c7d9cea2a8b27061c52a955360d1891e0
joshua-v-dev <joshua-v-dev@gmail.com>
2021-11-03 16:28:10 -0600

    gridtemplatecolumbs, gridcontainer minmax to 375

--- src/pages/MernProject.js
+++ src/pages/MernProject.js
@@ -88,7 +88,7 @@ export const Img = styled.img`
 
 export const GridContainer = styled.section`
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+	grid-template-columns: repeat(auto-fill, minmax(375px, 1fr));
 	padding: 3rem;
 	place-items: center;
 	column-gap: 2rem;
