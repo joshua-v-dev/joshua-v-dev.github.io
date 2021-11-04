d47a98ec75a3fd3ed1a37115bffa8ec89847b818
joshua-v-dev <joshua-v-dev@gmail.com>
2021-11-03 19:35:21 -0600

    resize button

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -195,7 +195,6 @@ export const ButtonBack = styled.div`
 	}
 
 	@media ${(props) => props.theme.breakpoints.sm} {
-		width: 100%;
 		height: 32px;
 		font-size: 14px;
 		margin-bottom: ${({ alt }) => (alt ? '0' : '32px')};
