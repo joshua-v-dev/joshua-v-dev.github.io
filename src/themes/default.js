80f68df44c1d195ad2bc0be27fc55ea24796557d
joshua-v-dev <joshua-v-dev@gmail.com>
2021-11-12 02:36:42 -0600

    altered theme breakpoints

--- src/themes/default.js
+++ src/themes/default.js
@@ -14,8 +14,8 @@ export default {
 	},
 	// Breakpoints for responsive design
 	breakpoints: {
-		sm: 'screen and (max-width: 400px)',
-		md: 'screen and (max-width: 768px)',
+		sm: 'screen and (max-width: 550px)',
+		md: 'screen and (max-width: 770px)',
 		lg: 'screen and (max-width: 1024px)',
 		xl: 'screen and (max-width: 1280px)',
 	},
