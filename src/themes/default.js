510bb1dcea3a7bd056f217e02730439bba844a0d
joshua-v-dev <joshua-v-dev@gmail.com>
2021-11-15 16:01:08 -0600

    add default

--- src/themes/default.js
+++ src/themes/default.js
@@ -14,7 +14,7 @@ export default {
 	},
 	// Breakpoints for responsive design
 	breakpoints: {
-		sm: 'screen and (max-width: 550px)',
+		sm: 'screen and (max-width: 640px)',
 		md: 'screen and (max-width: 770px)',
 		lg: 'screen and (max-width: 1024px)',
 		xl: 'screen and (max-width: 1280px)',
