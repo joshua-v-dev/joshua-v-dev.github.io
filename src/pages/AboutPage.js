afe5e933536dd3929271bb02bba9eb810b40e87b
joshua-v-dev <joshua-v-dev@gmail.com>
2021-11-22 06:04:55 -0600

    removed copilot suggestions

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,23 +1,11 @@
-// next.js component for the About page of the app (see AboutPage.js)
-// grid layout of the page
 import React from 'react'
-import Link from 'next/link'
 
 const AboutPage = () => {
 	return (
-		<div className='container'>
-			<div className='row'>
-				<div className='col-12'>
-					<h1>About</h1>
-					<p>
-						This is a simple React app that uses the{' '}
-						<Link href='/' target='_blank' rel='noopener noreferrer'>
-							<a>next.js</a> framework.
-						</Link>
-					</p>
-				</div>
-			</div>
+		<div>
+			<h1>About</h1>
 		</div>
 	)
 }
+
 export default AboutPage
