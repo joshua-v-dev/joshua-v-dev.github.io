b0281f254a9aa6cc675386d608bd46f4e6ac883b
joshua-v-dev <joshua-v-dev@gmail.com>
2021-12-06 16:26:14 -0600

    headerstyles

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -3,17 +3,17 @@ import styled from 'styled-components'
 import '../../themes/default'
 
 export const Container = styled.div`
-	display: grid;
-	grid-template-columns: repeat(5, 1fr);
-	grid-template-rows: 1fr;
-	grid-column-gap: 2rem;
+	// display: grid;
+	// grid-template-columns: repeat(5, 1fr);
+	// grid-template-rows: 1fr;
+	// grid-column-gap: 2rem;
 	padding: 1rem;
 	padding-top: 2rem;
 
 	@media ${(props) => props.theme.breakpoints.sm} {
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-template-rows: repeat(2, 60px);
+		// display: grid;
+		// grid-template-columns: repeat(5, 1fr);
+		// grid-template-rows: repeat(2, 60px);
 		grid-column-gap: 0.5rem;
 		grid-row-gap: 0.5rem;
 	}
