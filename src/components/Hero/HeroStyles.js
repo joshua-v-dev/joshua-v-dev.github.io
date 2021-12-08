a9f2b56929fdd8685ccc8fc486e20b0beecac871
joshua-v-dev <joshua-v-dev@gmail.com>
2021-12-07 22:05:21 -0600

    animation hero section overlay

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
 
-export const LeftSection = styled.div`
-	width: 100%;
-	display: grid;
+export const HeroSection = styled.div`
+	width: 75vw;
+	position: relative;
+
 	@media ${(props) => props.theme.breakpoints.sm} {
 		width: 80%;
 		display: flex;
@@ -14,7 +15,43 @@ export const LeftSection = styled.div`
 		width: 100%;
 		display: flex;
 		flex-direction: column;
+		margin: 0 auto;
+	}
+`
 
+export const LeftSection = styled.div`
+	width: 100%;
+	position: relative;
+
+	@media ${(props) => props.theme.breakpoints.sm} {
+		width: 80%;
+		display: flex;
+		flex-direction: column;
+
+		margin: 0 auto;
+	}
+	@media ${(props) => props.theme.breakpoints.md} {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		margin: 0 auto;
+	}
+`
+export const RightSection = styled.div`
+	width: 100%;
+	position: absolute;
+
+	@media ${(props) => props.theme.breakpoints.sm} {
+		width: 80%;
+		display: flex;
+		flex-direction: column;
+
+		margin: 0 auto;
+	}
+	@media ${(props) => props.theme.breakpoints.md} {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
 		margin: 0 auto;
 	}
 `
