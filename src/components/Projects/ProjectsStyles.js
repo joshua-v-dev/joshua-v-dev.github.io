d096217552f48f44a52cf3e2d024e39f044f6e60
joshua-v-dev <joshua-v-dev@gmail.com>
2021-12-10 05:03:18 -0600

    changed min max gridcontqin4

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -20,7 +20,7 @@ export const GridContainer = styled.section`
 	display: grid;
 	grid-areas: 'BlogCard' 'Overlay';
 	width: 100%;
-	grid-template-columns: repeat(auto-fill, minmax(500px, 1fr));
+	grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
 	padding: 3rem;
 	place-items: center;
 	column-gap: 3rem;
