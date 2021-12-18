82d1cb93575d0b805c392fdbd68378d3af142015
joshua-v-dev <joshua-v-dev@gmail.com>
2021-12-17 18:49:33 -0600

    swipers component created, styles

---
+++ src/components/Swiper/Swipers.js
@@ -0,0 +1,55 @@
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle'
+import SwiperStyles from './SwipersStyles'
+
+// import styles bundle
+import 'swiper/css/bundle'
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'vertical',
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+})
+
+const Swipers = () => (
+	// init Swiper:
+
+	<SwiperStyles>
+		<div className='swiper'>
+			{/* <!-- Additional required wrapper --> */}
+			<div className='swiper-wrapper'>
+				{/* <!-- Slides --> */}
+				<div className='swiper-slide'>Slide 1</div>
+				<div className='swiper-slide'>Slide 2</div>
+				<div className='swiper-slide'>Slide 3</div>
+				...
+			</div>
+			{/* <!-- If we need pagination --> */}
+			<div className='swiper-pagination'></div>
+
+			{/* <!-- If we need navigation buttons --> */}
+			<div className='swiper-button-prev'></div>
+			<div className='swiper-button-next'></div>
+
+			{/* <!-- If we need scrollbar --> */}
+			<div clasNames='swiper-scrollbar'></div>
+		</div>
+	</SwiperStyles>
+)
+export default Swipers
---
+++ src/components/Swiper/SwipersStyles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const SwiperStyles = styled.div`
+	width: 600px;
+	height: 300px;
+`
