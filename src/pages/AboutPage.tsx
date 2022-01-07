840add27b54fda25519c77a97d807876b4f87240
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-06 22:59:58 -0500

    removed Layout comp from aboutpage and contactpage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Layout from '../layout/Layout'
 
 const AboutPage = () => {
 	return (
-		<Layout>
+		<>
 			<div>
 				<h1>About</h1>
 			</div>
-		</Layout>
+		</>
 	)
 }
 
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Layout from '../layout/Layout'
 
 const ContactPage = () => {
 	return (
-		<Layout>
+		<>
 			<div>
 				<h1>Contact</h1>
 			</div>
-		</Layout>
+		</>
 	)
 }
 
