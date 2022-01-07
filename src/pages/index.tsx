5916a44f022333e457183661acaadf0cde543ac3
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-07 07:04:00 -0500

    put src/ in path for tailwindconfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-// import 'react-devtools'
 import React from 'react'
-
 import Particles from '../components/Particles/index'
 import BackgroundAnimation from '../components/BackgroundAnimation/BackgroundAnimation'
 import Hero from '../components/Hero/Hero'
@@ -8,7 +6,6 @@ import Projects from '../components/Projects/Projects'
 import Acomplishments from '../components/Acomplishments/Acomplishments'
 import Technologies from '../components/Technologies/Technologies'
 import Timeline from '../components/TimeLine/TimeLine'
-import Layout from '../layout/Layout'
 import ImageCarousel from '../components/Slider/ImageCarousel'
 import GlassCard from '../components/GlassCard/GlassCard'
 
