0861253c4cb3fe3e3fc4463a8dc8776ed55b7934
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-11 16:07:30 -0500

    macro plugin removed babelrc

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
 	mode: 'jit',
-	purge: [
-		'./src/pages/**/*.{js,ts,jsx,tsx}',
-		'./src/components/**/*.{js,ts,jsx,tsx}',
-		'./public/**/*.html',
-	],
+	purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: true,
 	theme: {
 		extend: {},
