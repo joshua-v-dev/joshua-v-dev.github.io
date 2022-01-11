f8680e30e093bebf00867faf8291f03fcebc2b93
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-11 16:16:45 -0500

    added Head from next/head to app.tsx with meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import PageWithLayoutType from '../layout/PageWithLayout'
 import React, { PropsWithChildren } from 'react'
 import GlobalStyles from '../styles/globals'
+import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
 
 type AppLayoutProps = {
@@ -12,6 +13,9 @@ function MyApp({ Component, pageProps }: AppLayoutProps) {
 	const Layout = Component.layout || ((children) => <>{children}</>)
 	return (
 		<>
+			<Head>
+				<meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
+			</Head>
 			<GlobalStyles />
 			<Layout>
 				<Component {...pageProps} />
