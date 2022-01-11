ae2748b3601ecbd2229f09340b0529c7ca119d1a
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-11 06:11:57 -0500

    updated tailwind config, swapped out _document.tsx for nextjs docs suggestion, removed webpack package

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,57 +1,11 @@
-import React, { ReactElement } from 'react'
-import Document, {
-	Html,
-	Head,
-	Main,
-	NextScript,
-	DocumentInitialProps,
-	DocumentContext,
-} from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { DocumentContext } from 'next/document'
 
-// NEXT.JS CUSTOM DOCUMENT
-// https://nextjs.org/docs/advanced-features/custom-document
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx)
 
-export default class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-		const sheet = new ServerStyleSheet()
-		const originalRenderPage = ctx.renderPage
-
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App: any) => (props: any) => sheet.collectStyles(<App {...props} />),
-				})
-
-			const initialProps = await Document.getInitialProps(ctx)
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			}
-		} finally {
-			sheet.seal()
-		}
-	}
-
-	render(): ReactElement {
-		return (
-			<Html lang='en'>
-				<Head>
-					<link
-						href='https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap'
-						rel='stylesheet'
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
+		return initialProps
 	}
 }
+
+export default MyDocument
