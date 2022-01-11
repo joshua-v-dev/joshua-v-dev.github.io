b23e912e55989649b664b5096f5bbcf3b4e8f5fc
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-11 05:20:16 -0500

    replaced for standard custom app from nextjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,20 @@
-import Head from 'next/head'
-import 'twin.macro'
-import React from 'react'
+// import App from "next/app";
+import type { AppProps /*, AppContext */ } from 'next/app'
 
-// type ComponentProps = {
-// 	Component: React.FC<any>
-// 	pageProps: React.ReactElement
-// }
-function MyApp({
-	Component,
-	pageProps,
-}: {
-	Component: React.ComponentState
-	pageProps: React.ReactElement
-}): React.ReactElement {
-	return (
-		<>
-			<Head>
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-			</Head>
-
-			<Component {...pageProps} />
-		</>
-	)
+function MyApp({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />
 }
 
 // Only uncomment this method if you have blocking data requirements for
 // every single page in your application. This disables the ability to
 // perform automatic static optimization, causing every page in your app to
 // be server-side rendered.
-
-// MyApp.getInitialProps = async (appContext: AppContext) => {
-// calls page's `getInitialProps` and fills `appProps.pageProps`
-// 	const appProps = await MyApp.getInitialProps(appContext)
 //
-// 	return (
-// 		<React.StrictMode>
-// 			<Component {...appProps} />
-// 		</React.StrictMode>
-// 	)
+// MyApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+
+//   return { ...appProps }
 // }
 
 export default MyApp
