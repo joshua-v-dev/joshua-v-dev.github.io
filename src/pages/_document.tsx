9c3c3ad670916fed615e24ab68919ec29ee18a63
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-19 07:46:14 -0500

    changed document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,21 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, { DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
-		const initialProps = await Document.getInitialProps(ctx)
-		return { ...initialProps }
-	}
+		const originalRenderPage = ctx.renderPage
 
-	render() {
-		return (
-			<Html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		)
+		ctx.renderPage = () =>
+			originalRenderPage({
+				// useful for wrapping the whole react tree
+				enhanceApp: (App) => App,
+				// useful for wrapping in a per-page basis
+				enhanceComponent: (Component) => Component,
+			})
+
+		// Run the parent `getInitialProps`, it now includes the custom `renderPage`
+		const initialProps = await Document.getInitialProps(ctx)
+
+		return initialProps
 	}
 }
 
