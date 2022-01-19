50c9bf565c300c27577e4c8d35e3fe4437ebeee3
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-19 07:04:17 -0500

    reset the main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,7 @@
-import PageWithLayoutType from '../layout/PageWithLayout'
-import React, { PropsWithChildren } from 'react'
-import GlobalStyles from '../styles/globals'
-import Head from 'next/head'
-import 'tailwindcss/tailwind.css'
+import { AppProps } from 'next/app'
 
-type AppLayoutProps = {
-	Component: PageWithLayoutType
-	pageProps?: PropsWithChildren<PageWithLayoutType> //  <-- this is the important part , not sure if this is right
+function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />
 }
 
-function MyApp({ Component, pageProps }: AppLayoutProps) {
-	const Layout = Component.layout || ((children) => <>{children}</>)
-	return (
-		<>
-			<Head>
-				<meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
-			</Head>
-			<GlobalStyles />
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</>
-	)
-}
-
-export default MyApp
+export default App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,21 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
 		const initialProps = await Document.getInitialProps(ctx)
+		return { ...initialProps }
+	}
 
-		return initialProps
+	render() {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
 	}
 }
 
