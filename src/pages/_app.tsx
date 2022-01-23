aa7fd9e44d13d6ced2ffe43c60ac9a57b4ce7e50
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-23 04:23:03 -0500

    config, cleanup, scaffold and architecture

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 import 'tailwindcss/tailwind.css'
-import GlobalStyles from '../styles/GlobalStyles'
+import GlobalStyles from '../styles/globalStyles'
 
 function App({ Component, pageProps }: AppProps) {
 	return (
--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,35 +1,12 @@
+import React from 'react'
 import { createGlobalStyle } from 'styled-components'
-import tw, { GlobalStyles as BaseStyles } from 'twin.macro' //{theme} //
+import tw, { GlobalStyles as BaseStyles } from 'twin.macro'
 
-export const CustomStyles = createGlobalStyle`
-
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    outline: 0;
-  }
-   #root{
-        margin:0 auto;
-    }
-  html {
-    font-size: 62.5%;
-    scroll-behavior: smooth;
-
-  }
+const CustomStyles = createGlobalStyle`
   body {
-
-    cursor: default;
+   
     ${tw`antialiased`}
   }
-  
-  a {
-    text-decoration: none;
-  }
-  li{
-    list-style: none;
-  }
-
 `
 const GlobalStyles = () => (
 	<>
