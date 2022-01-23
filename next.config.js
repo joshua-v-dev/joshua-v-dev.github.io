9674d24c75b29071350b33d86394b8e62ec00197
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-22 23:47:46 -0500

    next config, npm i process

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
 			// don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
 			config.resolve.fallback = {
 				fs: false,
+				process: 'process/browser',
 			}
 		}
 
