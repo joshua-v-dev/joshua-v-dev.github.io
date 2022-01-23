b12218781ac64088770dde370a395b234714eb17
joshua-v-dev <joshua-v-dev@gmail.com>
2022-01-23 04:45:50 -0500

    tsconfig next auto adjust from build command, add next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+module.exports = {
+	webpack: (config) => {
+		// Unset client-side javascript that only works server-side
+		config.resolve.fallback = { fs: false, module: false, path: false }
+		return config
+	},
+}
