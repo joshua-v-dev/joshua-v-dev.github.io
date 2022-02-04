c106b09f67864850517afa242be3535fb8014313
joshua-v-dev <joshua-v-dev@gmail.com>
2022-02-04 03:17:45 -0500

    swapped default and theme tsx files for opposites dir

--- src/pages/themes/Theme.tsx
+++ src/pages/themes/Theme.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'styled-components'
-import theme from '../themes/default'
+import theme from '../styles/Default'
 
 const Theme = ({ children }: { children: React.ComponentState }) => (
 	<ThemeProvider theme={theme}>{...children}</ThemeProvider>
