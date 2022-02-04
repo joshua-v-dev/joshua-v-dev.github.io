5531c37b8f13cbbc9bb7b4eaa1247730a9f5f5c3
joshua-v-dev <joshua-v-dev@gmail.com>
2022-02-04 04:30:23 -0500

    removed theme dir, defaultStyles

--- src/pages/themes/Theme.tsx
+++
@@ -1,8 +0,0 @@
-import { ThemeProvider } from 'styled-components'
-import theme from '../styles/defaultStyles'
-
-const Theme = ({ children }: { children: React.ComponentState }) => (
-	<ThemeProvider theme={theme}>{children}</ThemeProvider>
-)
-
-export default Theme
