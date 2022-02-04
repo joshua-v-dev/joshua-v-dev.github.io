afbf081c99e22a266874528d9f63cc0c22c3c2f7
joshua-v-dev <joshua-v-dev@gmail.com>
2022-02-04 04:14:52 -0500

    removed spread op from children in theme.tsx

--- src/pages/themes/Theme.tsx
+++ src/pages/themes/Theme.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components'
 import theme from '../styles/defaultStyles'
 
 const Theme = ({ children }: { children: React.ComponentState }) => (
-	<ThemeProvider theme={theme}>{...children}</ThemeProvider>
+	<ThemeProvider theme={theme}>{children}</ThemeProvider>
 )
 
 export default Theme
