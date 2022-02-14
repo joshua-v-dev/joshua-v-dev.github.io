927c7173f15a52d1d6f13df4a7ae5446e44af415
joshua-v-dev <joshua-v-dev@gmail.com>
2022-02-14 10:39:47 -0500

    update netlify plugin, edit content in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     './src/pages//*.{js,jsx,ts,tsx}',
     './src/components//*.{js,jsx,ts,tsx}',
+    './src/database//*.{js,jsx,ts,tsx}',
+    './src/public//*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
