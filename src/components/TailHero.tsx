747cfc332c63f329e867d243d41a761b6f7ca63b
joshua-v-dev <joshua-v-dev@gmail.com>
2022-02-15 21:25:37 -0500

    closer

--- src/components/TailHero.tsx
+++ src/components/TailHero.tsx
@@ -82,7 +82,7 @@ export default function TailHero() {
           <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl font-extrabold tracking-tight text-indigo-500 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">JOSHUA VAUGHN</span>{' '}
+                <span className="block xl:inline">REAL MAGIC = CODE </span>{' '}
                 <span className="block text-purple-700 xl:inline">
                   Full Stack Web Developer
                 </span>
