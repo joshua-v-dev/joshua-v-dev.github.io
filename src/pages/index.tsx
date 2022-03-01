ec122cdc5d90f11371ffe13d578b56e94bc4b6c1
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-01 13:29:17 -0500

    jest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import Timeline from "../components/TimeLine";
 import Layout from "../layout/Layout";
 import ProjectsPage from "../components/ProjectsPage";
 import Header from "../components/Header";
-import "tw-elements";
 import ImageCarousel from "../components/ImageCarousel";
 
 const Home: React.FC = () => {
