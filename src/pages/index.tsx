76100d6873857be06f341032d683eba614b2b3e5
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-01 13:09:13 -0500

    npm i tw-elements, ts-jest dev dep

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Timeline from "../components/TimeLine";
 import Layout from "../layout/Layout";
 import ProjectsPage from "../components/ProjectsPage";
 import Header from "../components/Header";
+import "tw-elements";
 import ImageCarousel from "../components/ImageCarousel";
 
 const Home: React.FC = () => {
