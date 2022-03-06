734b92f657bf6bc6aee4967cf55884ad1d018964
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-06 14:46:53 -0500

    eslint rules/config

--- next.config.js
+++ next.config.js
@@ -15,3 +15,20 @@ module.exports = {
     domains: ["res.cloudinary.com"],
   },
 };
+
+// module.exports = {
+//   webpack: (config, options) => {
+//     config.module.rules.push({
+//       test: /\.mdx/,
+//       use: [
+//         options.defaultLoaders.babel,
+//         {
+//           loader: "@mdx-js/loader",
+//           options: pluginOptions.options,
+//         },
+//       ],
+//     });
+//
+//     return config;
+//   },
+// };
