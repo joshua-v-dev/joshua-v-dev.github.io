5392cfa6abfba4abf66657cb88d5c8bc40b145a6
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-07 12:14:30 -0500

    npm update ran

--- next.config.js
+++ next.config.js
@@ -15,20 +15,3 @@ module.exports = {
     domains: ["res.cloudinary.com"],
   },
 };
-
-// module.exports = {
-//   webpack: (config, options) => {
-//     config.module.rules.push({
-//       test: /\.mdx/,
-//       use: [
-//         options.defaultLoaders.babel,
-//         {
-//           loader: "@mdx-js/loader",
-//           options: pluginOptions.options,
-//         },
-//       ],
-//     });
-//
-//     return config;
-//   },
-// };
