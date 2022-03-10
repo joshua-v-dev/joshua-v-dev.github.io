d1a4490f25f9bf5ea5974ff687c41a3ff1d5d455
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-10 18:17:50 -0500

    changed next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,33 @@
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//     if (!isServer) config.resolve.fallback.fs = false;
+//
+//     const fileLoaderRule = config.module.rules.find(
+//       (rule) => rule.test && rule.test.test(".svg")
+//     );
+//
+//     fileLoaderRule.exclude = /\.svg$/;
+//
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       loader: require.resolve("@svgr/webpack"),
+//     });
+//
+//     return config;
+//   },
+//   images: {
+//     domains: ["res.cloudinary.com", "static.shuffle.dev"],
+//   },
+// };
 module.exports = {
   webpack: (config, { isServer }) => {
-    if (!isServer) config.resolve.fallback.fs = false;
-    const fileLoaderRule = config.module.rules.find(
-      (rule) => rule.test && rule.test.test(".svg")
-    );
-    fileLoaderRule.exclude = /\.svg$/;
-    config.module.rules.push({
-      test: /\.svg$/,
-      loader: require.resolve("@svgr/webpack"),
-    });
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
     return config;
   },
   images: {
