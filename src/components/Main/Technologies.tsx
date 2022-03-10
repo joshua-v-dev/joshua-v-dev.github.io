9eb874e8c939c52952865ded41addd8f5a95d5c3
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-10 02:51:40 -0500

    technologies styles

--- src/components/Main/Technologies.tsx
+++ src/components/Main/Technologies.tsx
@@ -3,13 +3,13 @@ import { features } from "../../database/database";
 
 export default function Technologies() {
   return (
-    <div className=" space-y-8 bg-transparent py-16">
-      <div className="mx-auto my-auto max-w-7xl  space-y-8 px-4 sm:px-6 lg:px-8">
-        <div className=" relative space-y-6 lg:text-center">
-          <h1 className=" text-6xl font-extrabold  uppercase tracking-wide text-indigo-700 brightness-125">
+    <div className="bg-transparent">
+      <div className="mx-auto max-w-7xl space-y-8 px-2 sm:px-4 lg:px-6">
+        <div className=" relative space-y-4 lg:text-center">
+          <h1 className=" text-6xl font-extrabold uppercase tracking-wide text-indigo-700 brightness-125">
             Technologies
           </h1>
-          <p className=" font-bold tracking-tight text-gray-50 sm:text-3xl">
+          <p className="space-y-4 font-bold tracking-tight text-gray-50 sm:text-3xl lg:mx-auto">
             A better way to Full Stack
           </p>
           <p className="max-w-2xl text-3xl font-normal text-gray-50 lg:mx-auto">
@@ -18,28 +18,24 @@ export default function Technologies() {
           </p>
         </div>
 
-        <div className="container  ">
-          <dl className="mx-auto md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-12 md:space-y-2">
-            {features.map((feature) => (
-              <div key={feature.id} className="relative  ">
-                <dt>
-                  <div className="absolute flex h-10 w-10 items-center justify-center rounded-md bg-indigo-600 text-gray-50">
-                    <feature.icon
-                      className="relative h-6 w-6 "
-                      aria-hidden="true"
-                    />
-                  </div>
-                  <p className="px-12 text-3xl font-extrabold text-indigo-600 brightness-125">
-                    {feature.name}
-                  </p>
-                </dt>
-                <dd className="p-6 text-2xl text-white">
-                  {feature.description}
-                </dd>
-              </div>
-            ))}
-          </dl>
-        </div>
+        <dl className="mx-auto grid grid-cols-2 gap-x-8 gap-y-12 space-y-2">
+          {features.map((feature) => (
+            <div key={feature.id} className="relative  ">
+              <dt>
+                <div className="absolute flex h-10 w-10 items-center justify-center rounded-md bg-indigo-600 text-gray-50">
+                  <feature.icon
+                    className="relative h-6 w-6 "
+                    aria-hidden="true"
+                  />
+                </div>
+                <p className="px-12 text-3xl font-extrabold text-indigo-600 brightness-125">
+                  {feature.name}
+                </p>
+              </dt>
+              <dd className="p-6 text-2xl text-white">{feature.description}</dd>
+            </div>
+          ))}
+        </dl>
       </div>
     </div>
   );
