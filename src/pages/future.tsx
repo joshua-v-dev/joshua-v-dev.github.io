b3dee03ade69949267ee28dcd5de0f9bcc64a1e0
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-11 08:32:22 -0500

    add storybook, new page content

--- src/pages/future.tsx
+++ src/pages/future.tsx
@@ -68,11 +68,165 @@ const FuturePage = () => {
           <li className="mb-1 px-10"><a className="block pl-8 py-4 text-xl text-gray-800 hover:bg-blueGray-50 rounded-xl" href="#">Contact</a></li>
         </ul>
       </div>
-      <div className="mt-auto px-10">
-        <div className="pt-6"><a className="block mb-4 py-4 px-12 text-gray-800 text-center font-bold border border-gray-50 hover:border-gray-100 rounded-full" href="#">Sign in</a><a className="block py-4 px-12 text-white text-center font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Sign up</a></div>
-        <p className="mt-6 mb-4 text-lg text-center">
-          <span>2021 &copy; Zospace. All rights reserved.</span>
+    
+    </nav>
+  </div>
+</section>
+<section>
+  <div className="pt-64 bg-cover bg-no-repeat">
+    <div className="py-12 lg:pt-64 bg-white lg:clip-path-triangle">
+      <div className="max-w-xl mx-auto text-center">
+        <div className="flex items-center justify-center mb-6 text-xs text-blue-500 font-semibold uppercase">
+          <a href="#">Home</a>
+          <span className="inline-block mx-2">
+            <svg className="text-blue-500 h-3 w-3" viewBox="0 0 8 12" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.875 0L0 1.875L4.125 6L0 10.125L1.875 12L7.875 6L1.875 0Z"></path>
+            </svg>
+          </span>
+          <a href="#">Blog</a>
+          <span className="inline-block mx-2">
+            <svg className="text-blue-500 h-3 w-3" viewBox="0 0 8 12" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1.875 0L0 1.875L4.125 6L0 10.125L1.875 12L7.875 6L1.875 0Z"></path>
+            </svg>
+          </span>
+          <a href="#">Article</a>
+        </div>
+        <h2 className="mb-6 text-5xl text-blue-800 font-bold font-heading">Lorem ipsum dolor sit amet consectutar</h2>
+        <div className="flex items-center justify-center">
+          <div className="mr-6">
+            <Image className="w-14 h-14 object-cover" src="https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80" alt=""></Image>
+          </div>
+          <div className="text-left">
+            <h3 className="mb-2 text-lg text-gray-500 font-semibold font-heading">Sara Sampton</h3>
+            <p className="text-blue-500 text-xs">10 Jun 2021 | 19:40</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="container px-4 mx-auto">
+    <div className="max-w-2xl mx-auto pb-24">
+      <p className="mb-6 text-lg text-gray-500">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque eveniet est laborum exercitationem!</p>
+      <p className="mb-6 text-lg text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor illo ipsa magni dolore doloribus? Velit laboriosam est nostrum ducimus, amet enim sed quidem excepturi?</p>
+      <p className="mb-6 text-lg text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur aperiam ipsum vel facilis, quibusdam velit. Et, ipsam repellat? Possimus id exercitationem aut voluptatum cumque, nemo quaerat officiis accusamus.</p>
+      <p className="mb-6 text-lg text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor illo ipsa magni dolore doloribus? Velit laboriosam est nostrum ducimus, amet enim sed quidem excepturi?</p>
+    </div>
+  </div>
+</section>
+<section className="skewed-bottom-right">
+  <nav className="relative px-6 py-6 flex justify-between items-center bg-gray-900">
+    <a className="text-white text-3xl font-bold leading-none" href="#">
+      <Image className="h-12" src="atis-assets/logo/atis/atis-color-white.svg" alt="" width="auto"></Image>
+    </a>
+    <div className="lg:hidden">
+      <button className="navbar-burger flex items-center text-white p-3">
+        <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <title>Mobile menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+        </svg>
+      </button>
+    </div>
+    <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2  lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+      <li><a className="text-sm text-gray-300 hover:text-white" href="#">Start</a></li>
+      <li className="text-gray-800">
+        <svg className="w-4 h-4 current-fill" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+        </svg>
+      </li>
+      <li><a className="text-sm text-white font-bold" href="#">About Us</a></li>
+      <li className="text-gray-800">
+        <svg className="w-4 h-4 current-fill" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+        </svg>
+      </li>
+      <li><a className="text-sm text-gray-300 hover:text-white" href="#">Services</a></li>
+      <li className="text-gray-800">
+        <svg className="w-4 h-4 current-fill" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+        </svg>
+      </li>
+      <li><a className="text-sm text-gray-300 hover:text-white" href="#">Platform</a></li>
+      <li className="text-gray-800">
+        <svg className="w-4 h-4 current-fill" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+        </svg>
+      </li>
+      <li><a className="text-sm text-gray-300 hover:text-white" href="#">Testimonials</a></li>
+    </ul>
+    <a className="hidden lg:inline-block py-2 px-6 bg-pink-500 hover:bg-pink-600 text-sm text-white font-bold rounded-l-xl rounded-t-xl transition duration-200" href="#">Contact Us</a>
+  </nav>
+  <div className="bg-gray-900 pt-12 lg:pt-20 pb-20 radius-for-skewed">
+    <div className="container mx-auto px-4">
+      <div className="flex flex-wrap -mx-4">
+        <div className="w-full lg:w-1/2 px-4 mb-12 md:mb-20 lg:mb-0 flex items-center">
+          <div className="w-full text-center lg:text-left">
+            <div className="max-w-md mx-auto lg:mx-0">
+              <h2 className="mb-3 text-4xl lg:text-5xl text-white font-bold">
+                <span>Build &amp; Launch without</span>
+                <span className="text-purple-600">problems</span>
+              </h2>
+            </div>
+            <div className="max-w-sm mx-auto lg:mx-0">
+              <p className="mb-6 text-gray-400 leading-loose">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque efficitur nisl sodales egestas lobortis.</p>
+              <div><a className="inline-block mb-3 lg:mb-0 lg:mr-3 w-full lg:w-auto py-2 px-6 leading-loose bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-l-xl rounded-t-xl transition duration-200" href="#">Get Started</a><a className="inline-block w-full lg:w-auto py-2 px-6 leading-loose text-white font-semibold bg-gray-900 border-2 border-gray-700 hover:border-gray-600 rounded-l-xl rounded-t-xl transition duration-200" href="#">How it works</a></div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 px-4 flex items-center justify-center">
+          <div className="relative" >
+            <Image className="h-128 w-full max-w-lg object-cover rounded-3xl md:rounded-br-none" src="https://images.unsplash.com/photo-1462826303086-329426d1aef5?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80" alt=""></Image>
+            <Image className="hidden md:block absolute"  src="atis-assets/elements/blue-up.svg" alt=""></Image>
+            <Image className="hidden md:block absolute"  src="atis-assets/elements/wing-purple-down.svg" alt=""></Image>
+            <Image className="hidden md:block absolute"  src="atis-assets/elements/bullets-dark-right.svg" alt=""></Image>
+            <Image className="hidden md:block absolute" src="atis-assets/elements/bullets-yellow-left.svg" alt=""></Image>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div className="mr-for-radius">
+    <svg className="h-8 md:h-12 lg:h-20 w-full text-gray-900" viewBox="0 0 10 10" preserveAspectRatio="none">
+      <polygon fill="currentColor" points="0 0 10 0 0 10"></polygon>
+    </svg>
+  </div>
+  <div className="hidden navbar-menu fixed top-0 left-0 bottom-0 w-5/6 max-w-sm z-50">
+    <div className="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+    <nav className="relative flex flex-col py-6 px-6 h-full w-full bg-white border-r overflow-y-auto">
+      <div className="flex items-center mb-8">
+        <a className="mr-auto text-3xl font-bold leading-none" href="#">
+          <Image className="h-10" src="atis-assets/logo/atis/atis-color-black.svg" alt="" width="auto"></Image>
+        </a>
+        <button className="navbar-close">
+          <svg className="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+          </svg>
+        </button>
+      </div>
+      <div>
+        <ul>
+          <li className="mb-1"><a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-purple-50 hover:text-purple-600 rounded" href="#">Start</a></li>
+          <li className="mb-1"><a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-purple-50 hover:text-purple-600 rounded" href="#">About Us</a></li>
+          <li className="mb-1"><a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-purple-50 hover:text-purple-600 rounded" href="#">Services</a></li>
+          <li className="mb-1"><a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-purple-50 hover:text-purple-600 rounded" href="#">Platform</a></li>
+          <li className="mb-1"><a className="block p-4 text-sm font-semibold text-gray-400 hover:bg-purple-50 hover:text-purple-600 rounded" href="#">Testimonials</a></li>
+        </ul>
+      </div>
+      <div className="mt-auto">
+        <div className="pt-6"><a className="block px-4 py-3 mb-3 text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-l-xl rounded-t-xl" href="#">Sign In</a><a className="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-pink-600 hover:bg-pink-700 rounded-l-xl rounded-t-xl" href="#">Sign Up</a></div>
+        <p className="my-4 text-xs text-center text-gray-400">
+          <span>&copy; 2020 All rights reserved.</span>
         </p>
+        <div className="text-center">
+          <a className="inline-block px-1" href="#">
+            <Image src="atis-assets/social/facebook-purple.svg" alt=""></Image>
+          </a>
+          <a className="inline-block px-1" href="#">
+            <Image src="atis-assets/social/twitter-purple.svg" alt=""></Image>
+          </a>
+          <a className="inline-block px-1" href="#">
+            <Image src="atis-assets/social/instagram-purple.svg" alt=""></Image>
+          </a>
+        </div>
       </div>
     </nav>
   </div>
