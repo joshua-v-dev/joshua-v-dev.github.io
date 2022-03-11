e29453e9863953c2807c0d2232f98546fccbf96c
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-11 16:02:21 -0500

    d

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  staticDirs: ['../public', '../static', { from: '../foo/assets', to: '/assets' }],
+  staticDirs: ['../public', '../static', { from: '../stories/assets', to: '/assets' }],
   "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
@@ -12,14 +12,15 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "webpack5",
-  },
+  
      typescript: {
     check: false,
     checkOptions: {},
-    reactDocgen: false,
+    reactDocgen: true,
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
   },
   }
+}
