e84bd1f5e1ba4bb3f2c43654922693dc9d25eaef
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-12 02:45:00 -0500

    re

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   plugins: {
-    "postcss-import": {},
-      "postcss-flexbugs-fixes": {},
-   
     tailwindcss: { config: "./tailwind.config.js" },
-    autoprefixer: { flexbox: 'no-2009'},
-    "postcss-preset-env": {
-      features: { 'nesting-rules': true },
-    },
+    autoprefixer: {},
     ...(process.env.NODE_ENV === "production" ? {} : {}),
   },
 };
