8164a377f7a12e8338d5e022a02d78bb3e857b25
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-11 20:12:46 -0500

    ran debugger

--- src/stories/pages/home.stories.tsx
+++ src/stories/pages/home.stories.tsx
@@ -1,12 +1,10 @@
 // /stories/pages/home.stories.jsx
 
 import Home from "../../pages/index";
-
-
 export const HomePage = () => <Home />
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  title: "Pages/Home",
-  component: HomePage,
-};
+
+export default function HomePageStories() {
+  "Pages/Home";
+  HomePage;
+}
 
