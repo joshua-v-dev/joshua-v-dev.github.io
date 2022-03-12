958b8be1100003bb69279c19ec80fe641b7562e7
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-12 00:25:46 -0500

    stitchesconfig in babelmacfos

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-	webpack: (config /*, { isServer } */ ) => {
-// 		if (!isServer) {
-// 			// don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-// 			config.resolve.fallback = {
-// 				fs: false,
-// 			}
-// 		
-// 
-// 		}
+	webpack: (config) => {
+
 		config.resolve.preferRelative = true
 		return config
 	},
