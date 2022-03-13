97768031280a9ffd1c55688ffdaa3cf3df574fdf
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-12 21:48:14 -0500

    npx depcheck fixes

--- .storybook/main.tsx
+++ .storybook/main.tsx
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    '../src/components/**/*.stories.mdx',
-    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/stories/**/*.stories.mdx',
+    '../src/stories/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-postcss'],
+  addons: ['@storybook/addon-links/register', '@storybook/addon-essentials/register', '@storybook/addon-postcss/register', '@storybook/addon-docs/register'],
 }
