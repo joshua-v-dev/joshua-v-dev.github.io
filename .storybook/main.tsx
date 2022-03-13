f22264a6e8b397f2c7baf432a6c1164a8be6b191
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-13 08:08:42 -0400

    story

--- .storybook/main.tsx
+++ .storybook/main.tsx
@@ -1,9 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|mdx|ts|tsx)'],
   addons: ['@storybook/addon-docs/register',  
-    '@storybook/addon-postcss',
+    '@storybook/addon-postcss/register',
    {
-     name: '@storybook/addon-postcss',
+     name: '@storybook/addon-postcss/register',
     options: {
       postcssLoaderOptions: {
         implementation: require('postcss'),
