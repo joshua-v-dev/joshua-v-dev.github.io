f4e35e87438ab02aca6eb02d9c0bbf06ba1709cf
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-14 18:47:48 -0400

    config refactor

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
 
-const withTwin = require('next-twin');
-  withTwin({
+
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+  const withTwin = require('./next-twin.js')
+  module.exports =  withTwin({
+    images: {
+    domains: ['res.cloudinary.com'],
+  },
     experimental: {
       // Enables the styled-components SWC transform
       styledComponents: true,
-    },
-  })
-
-  // ATTENTION: because of https://github.com/vercel/next.js/issues/21679#issuecomment-771941447
-  
-
-module.exports = async ( withTwin, ({  NextConfig }) => {
- /**
- * @type {import('next').NextConfig}
- */
-  const nextConfig = {
-    /* config options here */
-    future: { webpack5: true },
-  }
-  return nextConfig
-})(withTwin);
-
+    }
+  }) 
