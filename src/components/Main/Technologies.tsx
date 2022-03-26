e7a419655e2b7b9cda657a6a5e5345ba0e2cfdd8
joshua-v-dev <joshua-v-dev@gmail.com>
2022-03-26 16:06:50 -0400

    work

--- src/components/Main/Technologies.tsx
+++ src/components/Main/Technologies.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { features } from "../../database/database";
 
-export default function Technologies() {
+export default function Technologies ()
+{
   return (
-    <div className=" bg-transparent py-64">
-      <div className="mx-auto max-w-7xl space-y-8 px-2 sm:px-4 lg:px-6">
+    <div className="grid grid-flow-row bg-transparent py-16">
+      <div className="mx-auto max-w-7xl space-y-8  sm:px-4 ">
         <div className=" relative space-y-4 lg:text-center">
           <h1 className=" text-6xl font-extrabold uppercase tracking-wide text-indigo-700 brightness-125">
             Technologies
@@ -16,26 +17,27 @@ export default function Technologies() {
             Cross platform, cross browser, cross device. No more headaches. Just
             focus on building your app.
           </p>
-        </div>
 
-        <dl className="mx-auto grid grid-cols-2 gap-x-10 gap-y-12 space-y-2 space-x-2">
-          {features.map((feature) => (
-            <div key={feature.id} className="relative  ">
-              <dt>
-                <div className="absolute flex h-10 w-10 items-center justify-center rounded-md bg-indigo-600 text-gray-50">
-                  <feature.icon
-                    className="relative h-6 w-6 "
-                    aria-hidden="true"
-                  />
-                </div>
-                <p className="px-12 text-3xl font-extrabold text-indigo-600 brightness-125">
-                  {feature.name}
-                </p>
-              </dt>
-              <dd className="p-6 text-2xl text-white">{feature.description}</dd>
-            </div>
-          ))}
-        </dl>
+
+          <dl className=" grid grid-cols-2 grid-flow-row-dense gap-x-8 gap-y-8 space-y-2 space-x-2">
+            { features.map( ( feature ) => (
+              <div key={ feature.id } className="relative  ">
+                <dt>
+                  <div className="absolute grid h-10 w-10 items-center justify-center rounded-md bg-indigo-600 text-gray-50">
+                    <feature.icon
+                      className="relative h-6 w-6 "
+                      aria-hidden="true"
+                    />
+                  </div>
+                  <p className="px-12 text-3xl font-extrabold text-indigo-600 brightness-125">
+                    { feature.name }
+                  </p>
+                </dt>
+                <dd className="p-6 text-2xl text-white">{ feature.description }</dd>
+              </div>
+            ) ) }
+          </dl>
+        </div>
       </div>
     </div>
   );
--- src/components/MultiComp/ImageCarousel.tsx
+++ src/components/MultiComp/ImageCarousel.tsx
@@ -3,41 +3,51 @@ import { featuredProducts } from "../../database/database";
 
 let slideInterval: any;
 
-const ImageCarousel = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+const ImageCarousel = () =>
+{
+  const [ currentIndex, setCurrentIndex ] = useState( 0 );
 
-  useEffect(() => {
-    slideInterval = setInterval(() => {
-      setCurrentIndex((currentIndex) => {
-        if (currentIndex === featuredProducts.length - 1) {
+  useEffect( () =>
+  {
+    slideInterval = setInterval( () =>
+    {
+      setCurrentIndex( ( currentIndex ) =>
+      {
+        if ( currentIndex === featuredProducts.length - 1 )
+        {
           return 0;
         }
         return currentIndex + 1;
-      });
-    }, 4000);
-  }, []);
+      } );
+    }, 4000 );
+  }, [] );
 
-  useEffect(() => {
-    return () => {
-      clearInterval(slideInterval);
+  useEffect( () =>
+  {
+    return () =>
+    {
+      clearInterval( slideInterval );
     };
-  }, []);
+  }, [] );
   return (
     <section className="container mx-auto grid items-center justify-center  rounded-3xl bg-teal-500 bg-opacity-30 py-24 px-48 brightness-125">
       <div className="flex flex-wrap items-center">
         <button
-          className="hidden h-16 w-16 rounded-full bg-indigo-500 p-5 transition duration-200 hover:bg-white lg:block"
-          onClick={() => {
-            setCurrentIndex((currentIndex) => {
-              if (currentIndex === 0) {
+          className="hidden h-16 w-16 rounded-full bg-indigo-600 p-5 transition duration-200 hover:bg-indigo-700 lg:block"
+          onClick={ () =>
+          {
+            setCurrentIndex( ( currentIndex ) =>
+            {
+              if ( currentIndex === 0 )
+              {
                 return featuredProducts.length - 1;
               }
               return currentIndex - 1;
-            });
-          }}
+            } );
+          } }
         >
           <svg
-            className="text-indigo-500 hover:text-indigo-500"
+            className="text-indigo-400 hover:text-indigo-500"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -52,33 +62,36 @@ const ImageCarousel = () => {
           </svg>
         </button>
         <div className="mx-auto mb-14 max-w-2xl text-center">
-          <h2 className="font-heading mb-4 text-3xl font-bold leading-tight text-white md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight">
+          <h2 className="font-heading mb-4 text-3xl font-extrabold leading-tight text-gray-200 md:text-4xl md:leading-tight lg:text-5xl lg:leading-tight">
             Check out my Blog!
           </h2>
-          <p className="mb-8 text-base leading-relaxed text-indigo-500 lg:text-xl lg:leading-relaxed">
+          <p className="mb-8 text-xl leading-relaxed text-indigo-500 lg:text-2xl lg:leading-relaxed">
             Checkout the latest news at my blog joshuasstories.tech. Keep up to
             date with me.
           </p>
           <a
-            className="inline-block rounded border border-white bg-white px-5 py-3 text-sm font-semibold text-indigo-500 transition duration-200 hover:bg-indigo-500"
-            href={featuredProducts[currentIndex]?.toString()}
+            className="inline-block rounded border border-gray-200 bg-indigo-600 px-5 py-3 text-sm font-semibold text-gray-200 transition duration-200 hover:bg-indigo-700"
+            href={ featuredProducts[ currentIndex ]?.toString() }
           >
             Learn More
           </a>
         </div>
         <button
-          className="hidden h-16 w-16 rounded-full bg-indigo-500 p-5 transition duration-200 hover:bg-white lg:block"
-          onClick={() => {
-            setCurrentIndex((currentIndex) => {
-              if (currentIndex === featuredProducts.length - 1) {
+          className="hidden h-16 w-16 rounded-full bg-indigo-600 hover:text-indigo-500 hover:bg-indigo-700 p-5 transition duration-200  lg:block"
+          onClick={ () =>
+          {
+            setCurrentIndex( ( currentIndex ) =>
+            {
+              if ( currentIndex === featuredProducts.length - 1 )
+              {
                 return 0;
               }
               return currentIndex + 1;
-            });
-          }}
+            } );
+          } }
         >
           <svg
-            className="text-indigo-500 hover:text-indigo-500"
+            className="text-indigo-400 hover:text-indigo-500"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
