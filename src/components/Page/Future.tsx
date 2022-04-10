994412b7845e83b40999563d92b6fa7ddf42471c
joshua-v-dev <joshua-v-dev@gmail.com>
2022-04-10 03:26:31 -0400

    ctamulti

--- src/components/Page/Future.tsx
+++ src/components/Page/Future.tsx
@@ -1,125 +1,12 @@
-
-
+import FutureHero from "components/Hero/FutureHero"
+import FutureMain from "components/Main/FutureMain"
 const Future = () =>
 {
     return (
 
         <main className="container rounded-t-full mx-auto py-2">
-            <section className=" overflow-hidden bg-transparent">
-                <div className=" mx-auto pt-5 md:pt-6 lg:pt-12">
-                    <div className=" flex flex-wrap">
-                        <div className=" px-4 2xl:w-2/5">
-                            <span className="bg-gradient-to-r from-purple-600 to-indigo-800 bg-clip-text text-5xl font-black text-transparent brightness-125">
-                                Two ways to start!
-                            </span>
-                            <h2 className=" my-5 max-w-lg text-5xl font-bold text-gray-200  lg:max-w-xl 2xl:text-6xl">
-                                Get perfect app for your project
-                            </h2>
-
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="skewed-bottom-right rounded-md my-5">
-                <div className="radius-for-skewed bg-gray-900 pt-12 pb-20 lg:pt-12">
-                    <div className="container mx-auto px-4">
-                        <div className=" flex flex-wrap">
-                            <div className="mb-8 flex w-full items-center px-4 md:mb-20 lg:mb-0 lg:w-1/2">
-                                <div className="w-full text-center lg:text-left">
-                                    <div className="mx-auto max-w-md lg:mx-0">
-                                        <h2 className="mb-3 text-4xl font-bold text-gray-200 lg:text-5xl">
-                                            <span>Build &amp; Launch without</span>
-                                            <span className="text-purple-600">problems</span>
-                                        </h2>
-                                    </div>
-                                    <div className="mx-auto max-w-sm lg:mx-0">
-                                        <p className="mb-4 leading-loose text-gray-400">
-                                            Easter Egg:
-                                            Everybody get up it&apos;s time to slam now
-                                            We got a real jam goin&apos; down
-                                            Welcome to the Space Jam
-                                            Here&apos;s your chance, do your dance
-                                            At the Space Jam Alright
-                                            Come on and slam, and welcome to the jam
-                                            Come on and slam, if you wanna jam!
-                                            ... Bugs Bunny Rules!
-                                        </p>
-                                        <div>
-                                            <a
-                                                className="mb-2 inline-block w-full rounded-r-xl rounded-t-xl bg-purple-600 py-2 px-6 font-semibold leading-loose text-gray-200 transition duration-200 hover:bg-purple-700 lg:mb-0 lg:mr-3 lg:w-auto"
-                                                href="#"
-                                            >
-                                                Get Started
-                                            </a>
-                                            <a
-                                                className="inline-block w-full rounded-r-xl rounded-t-xl border-2 border-gray-700 bg-gray-900 py-2 px-6 font-semibold leading-loose text-gray-200 transition duration-200 hover:border-gray-600 lg:w-auto"
-                                                href="#"
-                                            >
-                                                How it works
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="mr-for-radius">
-                    <svg
-                        className="h-6 w-12 bottom-3  text-gray-900 md:h-8 lg:h-12"
-                        viewBox="0 0 10 10"
-                        preserveAspectRatio="none"
-                    >
-                        <polygon fill="currentColor" points="0 0 10 0 0 10"></polygon>
-                    </svg>
-                </div>
-            </section>
-
-            <section className="lg:grid lg:grid-flow-col lg:grid-col-2 lg:my-12">
-
-                <div className="bg-cover bg-transparent bg-no-repeat pt-20">
-                    <div className=" bg-transparent my-3 ">
-                        <div className="mx-auto lg:space-y-8 max-w-2xl lg:max-w-5xl text-center">
-                            <div className="flex items-center justify-center">
-                                <div className="text-center lg:space-y-8">
-                                    <h3 className="  lg:text-7xl font-extrabold  text-indigo-800 brightness-150 leading-snug ">
-                                        Joshua Vaughn
-                                    </h3>
-                                    <p className="text-xs lg:text-5xl text-yellow-800 brightness-150 font-bold leading-loose">Full Stack Web Developer</p>
-                                </div>
-                            </div>
-                            <div className="flex items-center justify-center">
-                                <h2 className=" text-5xl lg:text-5xl font-bold  text-purple-800 brightness-150 leading-loose decoration-from-font">
-                                    The future is now!
-                                </h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="container mx-auto px-4">
-
-                    <div className="mx-auto max-w-2xl pb-12">
-
-                        <p className="mb-6 text-lg lg:text-2xl text-gray-500">
-                            These are the things that I am doing in the future for my web development projects.
-                        </p>
-                        <p className="mb-6 text-lg lg:text-2xl text-gray-500">
-                            Have you ever wondered where the internet is going? I have been working on this project for a while now and I am excited to see what the future holds.
-                            I believe we are at the pinnacle of what we can do with the internet. I am excited to see what the future holds.
-                        </p>
-                        <p className="mb-6 text-lg lg:text-2xl text-gray-500">
-                            Everytime I see a new project I am excited to see what the future holds. I am excited to see what the future holds.
-                        </p>
-                        <p className="mb-6 text-lg lg:text-2xl text-gray-500">
-                            With ever changing technology, I am excited to see what the future holds. I am excited to see what the future holds.
-                            Plus I am excited to see what the future holds.
-                        </p>
-                    </div>
-
-                </div>
-
-            </section>
+            <FutureHero />
+            <FutureMain />
         </main>
     )
 }
