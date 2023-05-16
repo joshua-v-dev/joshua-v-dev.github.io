2869879243cfd8c5f269027b642121ee618d9401
Joshua <joshua.v.dev@gmail.com>
2023-05-16 11:28:29 -0400

    prod buid

--- src/components/ParticlesContainer.tsx
+++ src/components/ParticlesContainer.tsx
@@ -1,10 +1,10 @@
 
 
-import { useCallback } from "react";
+// import { useCallback } from "react";
 import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
-import { Engine } from "tsparticles-engine";
-import { options } from "~/lib/constants";
+// import { loadFull } from "tsparticles";
+// import { Engine } from "tsparticles-engine";
+// import { options } from "~/lib/constants";
 
 const ParticlesContainer = () => {
   // const loadFull = useCallback((main: Engine) => {
@@ -14,27 +14,16 @@ const ParticlesContainer = () => {
   //   })();
   // }, []);
 
-  const customInit = useCallback((main: Engine) => {
-    loadFull(main);
-  }, []);
+  // const customInit = useCallback((main: Engine) => {
+  //   loadFull(main);
+  // }, []);
 
-  const customLoaded = useCallback((container: any) => {
-    console.log(container);
-  }, []);
-
-
-
-
-
+  // const customLoaded = useCallback((container: any) => {
+  //   console.log(container);
+  // }, []);
 
   return (
-    <Particles
-      id='tsparticles'
-      init={customInit}
-      loaded={customLoaded}
-      options={options}
-
-    />
+    <Particles id='tsparticles' />
 
 
   );
--- src/lib/sanity-server.ts
+++ src/lib/sanity-server.ts
@@ -1,8 +1,24 @@
-export const previewClient = require("@sanity/client").default({
+// export const previewClient = require("@sanity/client").default({
+//   projectId: process.env.SANITY_PROJECT_ID,
+//   dataset: process.env.SANITY_DATASET,
+//   useCdn: false,
+//   token: process.env.SANITY_API_TOKEN,
+// });
+import { createClient } from "@sanity/client";
+
+export const previewClient = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: process.env.SANITY_DATASET,
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
 });
+
+
+// export const sanityClient = sanityClient({
+//   projectId: process.env.SANITY_PROJECT_ID,
+//   dataset: process.env.SANITY_DATASET,
+//   useCdn: false,
+//   token: process.env.SANITY_API_TOKEN,
+// });
 
 // exports.previewClient = previewClient;
