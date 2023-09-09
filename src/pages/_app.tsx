80314b5163d4b8e63f5539c1cb951e1dc79bcc5c
Joshua <joshua.v.dev@gmail.com>
2023-09-09 16:32:16 -0400

    ready

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,38 @@
-import { Analytics } from "@vercel/analytics/react";
-import { Session } from "inspector";
-import { type AppType } from "next/app";
+import { Analytics } from "@vercel/analytics/react"
+import { Session } from "inspector"
+import { type AppType } from "next/app"
 
-import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "next-themes";
-import Container from "~/components/Container";
+import { SessionProvider } from "next-auth/react"
+import { ThemeProvider } from "next-themes"
+import Container from "~/components/Container"
 // import Footer from "~/components/Footer";
-import MobileMenu from "~/components/MobileMenu";
-import Nav from "~/components/Nav";
-import "../styles/main.css";
+import MobileMenu from "~/components/MobileMenu"
+import Nav from "~/components/Nav"
+import "../styles/main.css"
 
-const MyApp: AppType<{ session: Session }> = ({
+const MyApp: AppType<{ session: Session }> = ( {
   Component,
   pageProps: { session, ...pageProps },
-}) => {
+} ) => {
   return (
     <>
       <SessionProvider>
         <Container>
           <ThemeProvider attribute="class">
-            <MobileMenu />
-            <Nav />
+            <div className="bg-[url('/images/heroBg2.svg')]">
+              <MobileMenu />
+              <Nav />
 
-            <Component {...pageProps} />
+              <Component { ...pageProps } />
 
-            {/* <Footer /> */}
+              {/* <Footer /> */ }
+            </div>
           </ThemeProvider>
         </Container>
       </SessionProvider>
       <Analytics />
     </>
-  );
-};
+  )
+}
 
-export default MyApp;
+export default MyApp
