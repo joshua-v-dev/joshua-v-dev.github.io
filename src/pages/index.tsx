43712a0c5773c6702f8290a67ff7b7df1db09661
Joshua Vaughn <joshua.v.dev@gmail.com>
2023-10-18 23:53:14 -0400

    por

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import GlassTopPicks from "~/components/GlassTopPicks";
 import HomeCta from "~/components/HomeCta";
 import HomeHero from "~/components/HomeHero";
-import ParticlesContainer from "~/components/ParticlesContainer";
 
 const Home: React.FC = () => {
   return (
@@ -13,8 +12,6 @@ const Home: React.FC = () => {
         <GlassTopPicks />
         <div className="h-10" />
         <HomeCta />
-        <div className="h-10" />
-        <ParticlesContainer />
       </div>
     </>
   );
